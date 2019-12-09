242691caf4eef83b0c71e9abce21cea8618c36f9
unknown <zaslonka@gmail.com>
2019-12-09 15:26:56 +0100

    Adding Card component and updating create-react-app to the leatest

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { CardList } from './components/card-list/card-list.component';
+import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -12,19 +12,15 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users}));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
-      <div className="App">
-        <CardList name='Konrad'>
-          {this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+      <div className='App'>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
